65070cffec119117971f889330edd5c153aa20d5
Joshua Carp <jm.carp@gmail.com>
2015-05-24 17:07:53 -0400

    Update installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import re
+from setuptools import setup
 
 
 with open('README.rst') as readme_file:
@@ -15,26 +11,43 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    # TODO: put package requirements here
+    'aiohttp>=0.15.3',
+    'furl>=0.4.7',
+    'pyquery>=1.2.9',
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest',
 ]
+
+
+def find_version(fname):
+    """Attempts to find the version number in the file names fname.
+    Raises RuntimeError if not found.
+    """
+    version = ''
+    with open(fname, 'r') as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
 
 setup(
     name='pycrawl',
-    version='0.1.0',
-    description="Minimal web crawler build on asyncio",
+    version=find_version('pycrawl/__init__.py'),
+    description="Minimal web crawler built on asyncio",
     long_description=readme + '\n\n' + history,
     author="Joshua Carp",
     author_email='jm.carp@gmail.com',
     url='https://github.com/jmcarp/pycrawl',
-    packages=[
-        'pycrawl',
-    ],
-    package_dir={'pycrawl':
-                 'pycrawl'},
+    packages=['pycrawl'],
+    package_dir={'pycrawl': 'pycrawl'},
     include_package_data=True,
     install_requires=requirements,
     license="BSD",
@@ -45,13 +58,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
 )
